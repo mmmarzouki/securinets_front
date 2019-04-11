9d6cdd444c668180fb39defdf0b3411567a5c476
mmmarzouki <mmmarzouki@gmail.com>
2019-04-11 13:50:45 +0200

    added services

---
+++ src/app/services/submission.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Team} from '../model/team';
+import {Observable} from 'rxjs';
+import {Submission} from '../model/submission';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubmissionService {
+
+  private url:string="http://localhost:8080/submission/";
+
+  constructor(private http: HttpClient) { }
+
+  public findAll():Observable<Submission[]>{
+    return this.http.get<Submission[]>(this.url);
+  }
+  public findByTeam(id:number):Observable<Submission[]>{
+    return this.http.get<Submission[]>(this.url+id);
+  }
+}
---
+++ src/app/services/team.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Team} from '../model/team';
+import {Observable} from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamService {
+
+  private urlConnection:string="http://localhost:8080/login";
+
+  constructor(private http: HttpClient) { }
+
+  public login(team:Team):Observable<Team>{
+    return this.http.post<Team>(this.urlConnection,team,httpOptions);
+  }
+}
